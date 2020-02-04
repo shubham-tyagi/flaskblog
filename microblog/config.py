8cf7089891cc07f6e99d3956c4a3ae053df02f78
shubham-tyagi <raj.tyagi2000@gmail.com>
2020-02-04 15:33:15 +0530

    registration form added

--- microblog/config.py
+++ microblog/config.py
@@ -1,7 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get ('SECRET_KEY') or 'you-will-never-guess'
     # The first term looks for the value of an environment variable, also called SECRET_KEY. The second term, 
     # is just a hardcoded string.
     #  This is a pattern that you will see me repeat often for configuration variables.
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
