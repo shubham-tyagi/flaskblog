3aa60a5ad225c7f896dfb97e1934019983744db7
shubham-tyagi <raj.tyagi2000@gmail.com>
2021-11-10 16:06:12 +0530

    some errors removed
    Signed-off-by: shubham-tyagi <raj.tyagi2000@gmail.com>

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -59,4 +59,3 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
     # return 'es'
     
-
--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from app import db
-from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from hashlib import md5
